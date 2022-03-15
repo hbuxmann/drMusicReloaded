18f39f998a7d65e2e18516b10be5945dd46ddf33
hernan <hbuxmann@gmail.com>
2022-03-14 22:45:30 -0300

    ajustes menores - repo propio

--- database/config/config.js
+++ database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "hola",
+    "password": "root",
     "database": "drmusic",
     "host": "127.0.0.1",
     "dialect": "mysql"
